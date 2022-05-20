5562cdc6035f609c2e3b1938d8f7e0551895dc4b
Mayank-shrivastava <59906606+Mayank-shrivastava@users.noreply.github.com>
2022-05-21 00:10:58 +0530

    Time: 0 ms (100.00%), Space: 55.5 MB (5.48%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,18 +1,22 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // implementation of binary search algorithm(iterative);
-        int lo = 0;
-        int hi = nums.length-1;
-        while(lo <= hi) {
-            int mid = (lo+hi)/2;
-            if(nums[mid] == target) {
-                return mid;
-            } else if(nums[mid] > target) {
-                hi = mid-1;
-            } else {
-                lo = mid+1;
-            }
+        // (recursive binary search)
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+    
+    public int binarySearch(int[] nums, int target, int lo, int hi) {
+        if(hi < lo) {
+            return -1;
         }
-        return -1;
+        
+        int mid = (lo+hi)/2;
+        if(nums[mid] == target) {
+            return mid;
+        }
+        
+        if(nums[mid] > target) {
+            return binarySearch(nums,target,lo, mid-1);
+        }
+        return binarySearch(nums,target, mid+1, hi);
     }
 }
