c95c9b4974222dd3a18413ff559d21d6d9861c81
Mayank-shrivastava <59906606+Mayank-shrivastava@users.noreply.github.com>
2022-05-23 11:37:49 +0530

    Time: 5 ms (10.47%), Space: 54.1 MB (43.11%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -1,38 +1,28 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean isCol = false;
-        for(int i = 0; i < matrix.length; i++) {
-            if(matrix[i][0] == 0) {
-                isCol = true;
-            }
-            for(int j = 1; j < matrix[0].length; j++) {
+        int r = matrix.length;
+        int c = matrix[0].length;
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+
+        // traverse over the matrix
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                // store in rows and cols if matrix[i][j] == 0;
                 if(matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
-                    /* Initializing the flags in the same matrix
-                    */
-                    matrix[i][0] = 0; 
+                    rows.add(i); // add ith row
+                    cols.add(j); // add jth col
                 }
             }
         }
-        
-        for(int i =  1; i < matrix.length; i++) {
-            for(int j = 1; j < matrix[0].length; j++) {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+
+        // next iteration over matrix
+        for(int i = 0; i < r; i++) {
+            for(int j = 0; j < c; j++) {
+                if(rows.contains(i) || cols.contains(j)) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        
-        if(matrix[0][0] == 0) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                matrix[0][j] = 0;
-            }
-        }
-        
-        if(isCol) {
-            for(int i = 0; i < matrix.length; i++) {
-                matrix[i][0] = 0;
-            }
-        }
     }
-} 
+}
